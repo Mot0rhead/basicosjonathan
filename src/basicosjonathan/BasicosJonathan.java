c5b7779201fb08ffb6e22f480f5a6c50e0df1c0c
Jonathan Lopez Pastor <jonathanlopezp98@gmail.com>
2019-11-18 11:07:42 +0100

    logico hecho

--- src/basicosjonathan/BasicosJonathan.java
+++ src/basicosjonathan/BasicosJonathan.java
@@ -11,11 +11,19 @@ package basicosjonathan;
  */
 public class BasicosJonathan {
 
-    /**
-     * @param args the command line arguments
-     */
+    public static void logicos(){
+        boolean x=false;
+        int a=9,b=7;
+        if(a>b){
+            x=true;
+            System.out.println(a+" es mayor que "+b+" "+x);
+        }else{
+            
+            System.out.println(b+"es mayor que "+a+x);//en este caso x ya es igual a false y por eso no pongo x=false;
+        }
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        logicos();
     }
     
 }
